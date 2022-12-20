ae870052c8632a94ad3830f8f9b37af2a7579953
wxsatellite <1453085314@qq.com>
2022-12-20 14:06:21 +0800

    fix

--- web/v2/middleware/opentelemetry/opentelemetry.go
+++ web/v2/middleware/opentelemetry/opentelemetry.go
@@ -1,7 +1,9 @@
 package opentelemetry
 
 import (
+	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
 
 	v2 "github.com/wx-up/coding/web/v2"
 	"go.opentelemetry.io/otel/trace"
@@ -16,8 +18,19 @@ func (m *MiddlewareBuilder) Build() v2.Middleware {
 		return func(ctx *v2.Context) {
 			// 使用 request 的 context 作为父context
 			// 后续请求中的 span 都基于这个 context 生成
-			spanCtx, span := m.Tracer.Start(ctx.Req.Context(), "web")
+			reqCtx := ctx.Req.Context()
+			reqCtx = otel.GetTextMapPropagator().Extract(reqCtx, propagation.HeaderCarrier(ctx.Req.Header))
+			reqCtx, span := m.Tracer.Start(reqCtx, "unknown", trace.WithAttributes())
 			defer span.End()
+
+			span.SetAttributes(attribute.String("http.method", ctx.Req.Method))
+			span.SetAttributes(attribute.String("peer.hostname", ctx.Req.Host))
+			span.SetAttributes(attribute.String("http.url", ctx.Req.URL.String()))
+			span.SetAttributes(attribute.String("http.scheme", ctx.Req.URL.Scheme))
+			span.SetAttributes(attribute.String("span.kind", "server"))
+			span.SetAttributes(attribute.String("component", "web"))
+			span.SetAttributes(attribute.String("peer.address", ctx.Req.RemoteAddr))
+			span.SetAttributes(attribute.String("http.proto", ctx.Req.Proto))
 			span.SetAttributes(attribute.String("http.method", ctx.Req.Method))
 
 			// 只取 path 的一段，如果全部存储的话，当有人攻击时会给 trace 服务造成很大的压力
@@ -31,10 +44,11 @@ func (m *MiddlewareBuilder) Build() v2.Middleware {
 			// 将 request 中的 context 设置为 spanCtx
 			// 注意：这里复制 request 是一个性能很差的事情
 			// 优化：可以在 ctx 中新增一个 Ctx 字段
-			ctx.Req = ctx.Req.WithContext(spanCtx)
+			ctx.Req = ctx.Req.WithContext(reqCtx)
 
 			next(ctx)
 
+			// 设置响应状态码
 			span.SetAttributes(attribute.Int("http.status", ctx.RespStatusCode))
 		}
 	}
