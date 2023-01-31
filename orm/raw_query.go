0b3c6e5045b579a0898a54f8df998b3bd61ad659
wxsatellite <1453085314@qq.com>
2023-01-31 11:58:34 +0800

    原生查询构建

--- orm/raw_query.go
+++ orm/raw_query.go
@@ -1,7 +1,106 @@
 package orm
 
-// RawQuery 原生查询
-type RawQuery struct {
+import "context"
+
+// RawQuerier 原生查询
+// 用户直接使用原生sql或者通过Selector等构建SQL之后，最后都会通过 RawQuerier 和数据库交互，返回结果集
+type RawQuerier[T any] struct {
 	sql  string
 	args []any
+
+	sess Session
+
+	ms []Middleware
+}
+
+func (r *RawQuerier[T]) Build() (*Query, error) {
+	return &Query{SQL: r.sql, Args: r.args}, nil
+}
+
+func RawQuery[T any](sess Session, sql string, args ...any) *RawQuerier[T] {
+	core := sess.getCore()
+	return &RawQuerier[T]{
+		sql:  sql,
+		args: args,
+		sess: sess,
+		ms:   core.Middlewares(),
+	}
+}
+
+// Use 添加 RawQuerier 层面的 middleware
+func (r *RawQuerier[T]) Use(ms []Middleware) *RawQuerier[T] {
+	r.ms = append(r.ms, ms...)
+	return r
+}
+
+func (r *RawQuerier[T]) Get(ctx context.Context) (*T, error) {
+	return r.get(ctx)
+}
+
+func (r *RawQuerier[T]) GetMulti(ctx context.Context) ([]*T, error) {
+	panic("imp")
+}
+
+func (r *RawQuerier[T]) Exec(ctx context.Context) Result {
+	return Result{}
+}
+
+func (r *RawQuerier[T]) get(ctx context.Context) (*T, error) {
+	root := r.getHandler(r.sess)
+	for i := len(r.ms) - 1; i >= 0; i-- {
+		root = r.ms[i](root)
+	}
+
+	res := root(ctx, &QueryContext{
+		Type:    QueryTypeSelect,
+		Builder: r, // r 实现了 QueryBuilder 接口
+	})
+
+	if res.Err != nil {
+		return nil, res.Err
+	}
+
+	if res.Result != nil {
+		// 这里可能会 panic 例如用户自定义中间件时，给 Result 赋值为非 T 类型时
+		// 但是这是用户使用不当导致的，框架设计者可以不处理
+		return res.Result.(*T), nil
+	}
+	return nil, res.Err
+}
+
+func (r *RawQuerier[T]) getHandler(sess Session) Handler {
+	return func(ctx context.Context, qc *QueryContext) *QueryResult {
+		query, err := qc.Builder.Build()
+		if err != nil {
+			return &QueryResult{
+				Err: err,
+			}
+		}
+		rows, err := sess.queryContext(ctx, query.SQL, query.Args...)
+		if err != nil {
+			return &QueryResult{
+				Err: err,
+			}
+		}
+
+		if !rows.Next() {
+			return &QueryResult{
+				Err: ErrNoRows,
+			}
+		}
+
+		t := new(T)
+		model, err := sess.getCore().R().Get(t)
+		if err != nil {
+			return &QueryResult{
+				Err: err,
+			}
+		}
+		err = sess.getCore().ValCreator()(model, t).SetColumns(rows)
+
+		return &QueryResult{
+			Result: t,
+			Err:    err,
+		}
+	}
 }
--- orm/select.go
+++ orm/select.go
@@ -185,55 +185,13 @@ func (s *Selector[T]) buildColumns() error {
 
 // Get 查询单条数据
 func (s *Selector[T]) Get(ctx context.Context) (*T, error) {
-	var root Handler = func(ctx context.Context, qc *QueryContext) *QueryResult {
-		query, err := qc.Builder.Build()
-		if err != nil {
-			return &QueryResult{
-				Err: err,
-			}
-		}
-		rows, err := s.sess.queryContext(ctx, query.SQL, query.Args...)
-		if err != nil {
-			return &QueryResult{
-				Err: err,
-			}
-		}
-
-		if !rows.Next() {
-			return &QueryResult{
-				Err: ErrNoRows,
-			}
-		}
-
-		t := new(T)
-		err = s.sess.getCore().ValCreator()(s.model, t).SetColumns(rows)
-
-		return &QueryResult{
-			Result: t,
-			Err:    err,
-		}
+	query, err := s.Build()
+	if err != nil {
+		return nil, err
 	}
 
-	for i := len(s.ms) - 1; i >= 0; i-- {
-		root = s.ms[i](root)
-	}
-
-	res := root(ctx, &QueryContext{
-		Type:    QueryTypeSelect,
-		Builder: s,
-	})
-
-	if res.Err != nil {
-		return nil, res.Err
-	}
-
-	if res.Result != nil {
-		// 这里可能会 panic 例如用户自定义中间件时，给 Result 赋值为非 T 类型时
-		// 但是这是用户使用不当导致的，框架设计者可以不处理
-		return res.Result.(*T), nil
-	}
-	return nil, res.Err
-
+	// 最后也是委托给 原生查询
+	return RawQuery[T](s.sess, query.SQL, query.Args...).Use(s.ms).Get(ctx)
 }
 
 func (s *Selector[T]) GetMulti(ctx context.Context) ([]*T, error) {
